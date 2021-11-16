aca7ee57883172c3928176f062f464142573a6af
Deki97 <dejan97@libero.it>
2021-11-16 13:45:17 +0100

    done milestone 1

---
+++ milestone 1/js/script.js
@@ -0,0 +1,20 @@
+// Scrivi un programma che stampi i numeri da 1 a 100.
+
+// MILESTONE 1
+// Per i multipli di 3 stampi in console “Fizz” al posto del numero e per i multipli di 5 stampi in console Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
+
+for(let i = 1; i <= 100; i++) {
+
+    let FizzOrBuzzOrBoth = i.toString();
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        FizzOrBuzzOrBoth = "FizzBuzz";
+    } else if ( i % 5 === 0) {
+        FizzOrBuzzOrBoth = "Buzz";
+    } else if (i % 3 === 0) {
+        FizzOrBuzzOrBoth = "Fizz";
+    }
+
+    console.log(FizzOrBuzzOrBoth);
+}
